c7fd8eb072765b03fca26ec8c562947cad0febc6
wcargen <wcargen@gmail.com>
2013-12-09 14:33:30 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splitfare::Application.config.secret_token = '040780c3d68e3a7b481343f6ae593cb4b43b560c1cc9e2edf3748c976d1ff70ef279a5c2973228b06687bb41d3e8b3970ef65f85c66a2f7cd1bca6110d7631a2'
