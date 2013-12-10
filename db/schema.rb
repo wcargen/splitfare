d62ee807175a7cbefcfbbefddd9c2e98dc53cac1
wcargen <wcargen@gmail.com>
2013-12-10 14:13:46 -0800

    Changed phone_number data type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131210035107) do
+ActiveRecord::Schema.define(:version => 20131210220813) do
 
   create_table "location_rides", :force => true do |t|
     t.integer  "location_id"
@@ -54,22 +54,22 @@ ActiveRecord::Schema.define(:version => 20131210035107) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",                  :default => "", :null => false
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "phone_number"
+    t.integer  "phone_number",           :limit => 8
     t.float    "rating"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",                       :default => 0,  :null => false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
     t.boolean  "host"
   end
 
