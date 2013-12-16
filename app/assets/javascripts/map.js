73757031d5abe4a7f210be4e52f5fe0a4d21e723
wcargen <wcargen@gmail.com>
2013-12-15 20:58:37 -0800

    Added geolocation for map.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,27 +1,64 @@
-$(document).ready(function(){
+$(function(){
 
-// Google Map
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 13,
-    center: new google.maps.LatLng(37.7833, -122.4167)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
+  var map;
 
-// Google Places Autocomplete for Ride Addresses
-var ride_address = document.getElementById('ride_address');
-var ride_destination = document.getElementById('ride_destination');
-var options = {
-    types: [],
-    componentRestrictions: {country: 'us'}
-};
+  function initialize() {
+    var mapOptions = {
+      zoom: 17
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
-var autocomplete_address = new google.maps.places.Autocomplete(ride_address, options);
-var autocomplete_destination = new google.maps.places.Autocomplete(ride_destination, options);
+    // Try HTML5 geolocation
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
 
-google.maps.event.addDomListener(window, 'load', initialize);
+        var user_marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          title: 'Your location!'
+        });
 
+        map.setCenter(pos);
+      }, function() {
+        handleNoGeolocation(true);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleNoGeolocation(false);
+    }
+
+    // Google Places Autocomplete for Ride Addresses
+    var ride_address = document.getElementById('ride_address');
+    var ride_destination = document.getElementById('ride_destination');
+    var options = {
+        types: [],
+        componentRestrictions: {country: 'us'}
+    };
+
+    var autocomplete_address = new google.maps.places.Autocomplete(ride_address, options);
+
+    var autocomplete_destination = new google.maps.places.Autocomplete(ride_destination, options);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+      var content = 'Error: The Geolocation service failed.';
+    } else {
+      var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+      map: map,
+      position: new google.maps.LatLng(60, 105),
+      content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 });
